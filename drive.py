3a19b7765b94d52bb2a626fa71e3f464d06eb40a
Anton Beliaev <anton.beliaev@student.torrens.edu.au>
2023-11-27 13:30:41 +1030

    Initial version of training & driving

--- drive.py
+++ drive.py
@@ -1,12 +1,54 @@
-import eventlet.wsgi
-import socketio
-from flask import Flask
+import base64
+import glob
+import os
+import sys
+from getopt import getopt
+from io import BytesIO
 
-debug = True
-file = None
+import eventlet.wsgi
+import numpy as np
+import socketio
+from PIL import Image
+from flask import Flask
+from keras.models import load_model
+
+from model import crop, cropped_width, cropped_height, origin_colours
+
+debug: bool = True
+
+file: str = ""
+speed_limit: int = 15
+
+cli_longopts = [
+    "debug",
+    "file=",
+]
+
+opts, args = getopt(sys.argv[1:], shortopts="", longopts=cli_longopts)
+
+for opt, arg in opts:
+    if opt == "debug":
+        debug = True
+    elif opt == "--file":
+        file = arg
 
 server = socketio.Server()
 app = Flask(import_name="ISY503 A3 Group 5")
+
+
+def choose_throttle(speed: float) -> float:
+    delta = 0.25
+    throttle = 0.0
+
+    if abs(speed_limit - speed) > delta:
+        throttle = (1.0 - speed / speed_limit) * 1.5
+
+    if 0 <= throttle < 0.09:
+        throttle = 0.09
+    elif -0.09 < throttle < 0:
+        throttle = -0.09
+
+    return round(throttle, 3)
 
 
 def event_connect_handler(sid: str, _: dict):
@@ -14,15 +56,24 @@ def event_connect_handler(sid: str, _: dict):
 
 
 def event_telemetry_handler(sig: str, msg: dict):
-    mode = "manual"
-    control = {}
+    mode: str = "manual"
+    control: dict[str, str] = {}
 
     if msg:
         mode = "steer"
 
+        img_center = msg["image"]
+        speed = float(msg["speed"])
+        img = Image.open(BytesIO(base64.b64decode(img_center)))
+        img = crop(img)
+
+        image_array = np.asarray(img).reshape([1, cropped_height(), cropped_width(), origin_colours])
+
+        predicted = model.predict(image_array, verbose=0)
+
         control = {
-            "steering_angle": "0",
-            "throttle": "1",
+            "steering_angle": round(float(predicted[0][0]), 3).__str__(),
+            "throttle": choose_throttle(speed).__str__(),
         }
 
         if debug is True:
@@ -34,6 +85,14 @@ def event_telemetry_handler(sig: str, msg: dict):
 server.on('connect', event_connect_handler)
 server.on('telemetry', event_telemetry_handler)
 
-if __name__ == '__main__':
-    app = socketio.Middleware(server, app)
-    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
+if file == "":
+    models = glob.glob("./model-2023-*")
+    models.sort(reverse=False)
+    file = os.path.basename(models[-1])
+
+print("Model %s is chosen to be used as a target" % file)
+
+model = load_model(file, safe_mode=False)
+
+app = socketio.Middleware(server, app)
+eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
---
+++ model.py
@@ -0,0 +1,159 @@
+import sys
+from datetime import datetime as dt
+from getopt import getopt
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from PIL import Image
+from keras.callbacks import Callback, ModelCheckpoint
+from keras.layers import Conv2D, Dense, Flatten, Lambda, MaxPooling2D
+from keras.losses import MSE
+from keras.models import Sequential
+from keras.optimizers import Adam
+
+print(tf.__version__)
+
+debug: bool = True
+
+sources: list[str] = []
+extra_steering: float = 0.15
+validation_data_percent: float = 0.3
+
+origin_image_width: int = 320
+origin_image_height: int = 160
+origin_colours: int = 3
+crop_left: int = 0
+crop_top: int = 55
+crop_right: int = 0
+crop_bottom: int = 25
+
+cli_longopts: list[str] = [
+    "debug",
+    "sources=",
+]
+
+opts, args = getopt(sys.argv[1:], shortopts="", longopts=cli_longopts)
+
+for opt, arg in opts:
+    if opt == "debug":
+        debug = True
+    elif opt == "--sources":
+        sources = arg.split(',')
+
+
+def crop(img: Image) -> Image:
+    return img.crop((
+        crop_left,
+        crop_top,
+        origin_image_width - crop_right,
+        origin_image_height - crop_bottom,
+    ))
+
+
+def cropped_height() -> int:
+    return origin_image_height - crop_top - crop_bottom
+
+
+def cropped_width() -> int:
+    return origin_image_width - crop_left - crop_right
+
+
+def get_driving_logs() -> pd.DataFrame:
+    clear_data_list: list[pd.DataFrame] = []
+
+    for source in sources:
+        csv = pd.read_csv(
+            source + '/driving_log.csv',
+            delimiter=',',
+            names=['center', 'left', 'right', 'steering'],
+            usecols=[0, 1, 2, 3],
+        )
+
+        for column in ['center', 'left', 'right']:
+            csv[column] = csv[column].map(lambda path: "%s/%s" % (source, "/".join(path.split('/')[-2:])))
+
+        clear_data_list.append(csv)
+
+    return pd.concat(clear_data_list)
+
+
+def get_datasets_from_logs(logs: pd.DataFrame) -> (np.ndarray, np.ndarray, np.ndarray, np.ndarray):
+    train_x: list[np.ndarray] = []
+    train_y: list[np.ndarray] = []
+
+    val_x: list[np.ndarray] = []
+    val_y: list[np.ndarray] = []
+
+    for index, row in logs.iterrows():
+        steering = row['steering']
+
+        rand = np.random.rand()
+
+        if rand < 1 / 3:
+            image = Image.open(row['center'])
+        elif rand < 2 / 3:
+            image = Image.open(row['left'])
+            steering += extra_steering
+        else:
+            image = Image.open(row['right'])
+            steering -= extra_steering
+
+        image = image.crop((
+            crop_left,
+            crop_top,
+            origin_image_width - crop_right,
+            origin_image_height - crop_bottom,
+        ))
+
+        if np.random.rand() > validation_data_percent:
+            train_x.append(np.asarray(image))
+            train_y.append(steering)
+        else:
+            val_x.append(np.asarray(image))
+            val_y.append(steering)
+
+    return np.asarray(train_x), np.asarray(train_y), np.asarray(val_x), np.asarray(val_y)
+
+
+def build_model() -> Sequential:
+    model = Sequential()
+
+    model.add(Lambda(lambda x: x / 255, input_shape=(cropped_height(), cropped_height(), origin_colours)))
+    model.add(Conv2D(filters=32, kernel_size=(5, 5), strides=(2, 2), activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(filters=32, kernel_size=(3, 3), activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(filters=32, kernel_size=(3, 3), activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(filters=32, kernel_size=(3, 3), activation="relu"))
+    model.add(Flatten())  # 512 pixels
+    model.add(Dense(units=100, activation="relu"))
+    model.add(Dense(units=1))
+
+    model.compile(loss=MSE, optimizer=Adam())
+
+    return model
+
+
+def model_callback_list() -> list[Callback]:
+    list: list[Callback] = []
+
+    list.append(
+        ModelCheckpoint(
+            "model-{}.h5".format(dt.now().strftime("%Y-%m-%d-%H-%M-%S")),
+            monitor="val_loss",
+            mode="min",
+            verbose=1,
+            save_best_only=True,
+        ),
+    )
+
+    return list
+
+
+if __name__ == '__main__':
+    logs = get_driving_logs()
+    train_X, train_Y, val_X, val_Y = get_datasets_from_logs(logs)
+    model = build_model()
+    model.fit(train_X, train_Y, validation_data=(val_X, val_Y), epochs=5, callbacks=model_callback_list())
